e23fecec49c85ea61e449501cb379a9bed69e310
Rafael Biriba <biribarj@gmail.com>
2020-07-21 16:38:39 +0200

    Adicionando mais conteúdo no 4a

--- 4/gabarito_2.rb
+++ 4/gabarito_2.rb
@@ -1,14 +1,14 @@
-dolar = 5
-euro = 6
+dolar = 5.00
+euro = 6.00
 
-cadeira = 10
-mesa = 20
+cadeira = 10.00
+mesa = 20.00
 
-cadeira_em_dolar = cadeira * dolar
-mesa_em_dolar = mesa * dolar
+cadeira_em_dolar = cadeira / dolar
+mesa_em_dolar = mesa / dolar
 
-cadeira_em_euro = cadeira * euro
-mesa_em_euro = mesa * euro
+cadeira_em_euro = cadeira / euro
+mesa_em_euro = mesa / euro
 
 puts "A cadeira custa em dolar #{cadeira_em_dolar} e custa em euro #{cadeira_em_euro}"
 
