e46932264464b7624821a695b93534eb8055c08b
Rafael Biriba <biribarj@gmail.com>
2020-07-21 22:28:41 +0200

    Update gabarito_6.rb

--- 5/gabarito_6.rb
+++ 5/gabarito_6.rb
@@ -1,12 +1,18 @@
 texto = "banana#morango#abacaxi!limao!mamao"
 
-resultado = texto.split("#")
-# ["banana", "morango", abacaxi!limao!mamao]
+resultado1 = texto.split("#")
+#puts "#{resultado1}"
+resultado2 = resultado1[2].split("!")
+#puts "#{resultado2}"
 
-resultado2 = resultado[2].split("!")
-# ["abacaxi", "limao", "mamao"]
-
-resultado_final = [resultado[0], resultado[1], resultado2[0], resultado2[1], resultado2[2]]
-#["banana", "morango", "abacaxi", "limao", "mamao"]
-
+resultado_final = [
+  resultado1[0],
+  resultado1[1],
+  resultado2[0],
+  resultado2[1],
+  resultado2[2]
+]
 puts "Resultado: #{resultado_final}"
+
+# Resultado: ["banana", "morango", "abacaxi", "limao", "mamao"]
+
