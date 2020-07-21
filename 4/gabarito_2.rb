1db0da376fe772108b5ee1d223d4080b13451efb
Rafael Biriba <biribarj@gmail.com>
2020-07-21 17:34:29 +0200

    Aula 4

--- 4/gabarito_2.rb
+++ 4/gabarito_2.rb
@@ -5,11 +5,13 @@ cadeira = 10.00
 mesa = 20.00
 
 cadeira_em_dolar = cadeira / dolar
-mesa_em_dolar = mesa / dolar
-
 cadeira_em_euro = cadeira / euro
+
+mesa_em_dolar = mesa / dolar
 mesa_em_euro = mesa / euro
 
-puts "A cadeira custa em dolar #{cadeira_em_dolar} e custa em euro #{cadeira_em_euro}"
+puts "A cadeira em dolar custa #{cadeira_em_dolar}"
+puts "A cadeira em euro custa#{cadeira_em_euro}"
 
-puts "A mesa custa em dolar #{mesa_em_dolar} e custa em euro #{mesa_em_euro}"
+puts "A mesa em dolar custa #{mesa_em_dolar}"
+puts "A mesa em euro custa #{mesa_em_euro}"
