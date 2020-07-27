65b3a06d6e89adbb3c5d4d48f76697e69cea324b
Rafael Biriba <biribarj@gmail.com>
2020-07-27 21:16:25 +0200

    Adding class 8

---
+++ 8/gabarito_4.rb
@@ -0,0 +1,10 @@
+agenda = [{nome: "Rafael", telefone: "5566"}, {nome: "Rodolfo", telefone: "9988"} , {nome: "Romário", telefone: "2299"}]
+procurar_por = "Rodolfo"
+
+agenda.each do |contato|
+  if contato[:nome] == procurar_por
+    puts "Nome: #{contato[:nome]} - Telefone: #{contato[:telefone]}"
+  else
+    next
+  end
+end
